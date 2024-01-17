6f0c0647b45ddfccb0cccd580997ef0bab5e02f9
Бобик <Atyg76ferti@gmail.com>
2024-01-17 22:51:35 +0300

    млжет когданиубдь доделаю

---
+++ CFR/Commands.cs
@@ -0,0 +1,27 @@
+﻿namespace CFR
+{
+    internal class Commands
+    {
+        public enum CommandsLists
+        {
+
+        }
+        public string[] ReadCommand(string comm)
+        {
+            comm = comm.ToLower();
+            string[] Command = comm.Split(' ');
+            return Command;
+        }
+
+        public void CommandComplit(string[] comm, CommandsLists CLists)
+        {
+            MainMenu mainMenu = new MainMenu();
+            switch (CLists)
+            {
+                //    case :
+
+                //        break;
+            }
+        }
+    }
+}
---
+++ CFR/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using static CFR.Messeg;
+global using static CFR.Info;
+global using static CFR.MainMenu;
+//global using static CFR.Language;
