0078add1ff887fa757065bf769ad64148cf6ef5a
Бобик <Atyg76ferti@gmail.com>
2024-01-17 22:54:13 +0300

    v0.1 build170124

---
+++ CFR/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CFR
+{
+    internal class Program
+    {
+        static public int Version = 1;
+        static public int Build = 170124;
+        static void Main()
+        {
+            Console.Title = $"CFR v0.{Version} build {Build}";
+            StartMenu();
+        }
+
+
+    }
+}
